4a68ff01dfd3c39777d691dc228888aeb5e18d1b
Wahed Koudsi <82679451+Wahed-koudsi2001@users.noreply.github.com>
2024-04-18 03:09:30 +0200

    Uploads new Design

---
+++ assets/js/script.js
@@ -0,0 +1,120 @@
+'use strict';
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+/**
+ * toggle navbar
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  navToggler.classList.toggle("active");
+}
+
+addEventOnElem(navToggler, "click", toggleNavbar);
+
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+  navToggler.classList.remove("active");
+}
+
+addEventOnElem(navbarLinks, "click", closeNavbar);
+
+
+
+/**
+ * header active
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+
+
+
+// Send FORM
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("my-form-status");
+  var form = event.target;
+  var data = new FormData(form);
+  fetch(form.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      'Accept': 'application/json'
+    }
+  }).then(response => {
+    if (response.ok) {
+      status.innerHTML = "Thanks for your submission!";
+      form.reset();
+    } else {
+      response.json().then(data => {
+        if (Object.hasOwn(data, 'errors')) {
+          status.innerHTML = data["errors"].map(error => error["message"]).join(", ");
+        } else {
+          status.innerHTML = "Oops! There was a problem submitting your form";
+        }
+      });
+    }
+  }).catch(error => {
+    status.innerHTML = "Oops! There was a problem submitting your form";
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const aboutSection = document.getElementById("about");
+  const nums = document.querySelectorAll(".nums");
+
+  const options = {
+    threshold: .5
+  };
+
+  const observer = new IntersectionObserver(function (entries) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        nums.forEach(num => {
+          const targetNumber = +num.innerText.replace(/\D/g, "");
+          let count = 0;
+          const countInterval = setInterval(() => {
+            if (count < targetNumber) {
+              count += Math.ceil(targetNumber / 150);
+              num.textContent = "+" + count;
+            } else {
+              num.textContent = "+" + targetNumber;
+              clearInterval(countInterval);
+            }
+          }, 20);
+        });
+        observer.unobserve(aboutSection);
+      }
+    });
+  }, options);
+
+  if (aboutSection) {
+    observer.observe(aboutSection);
+  } else {
+    console.error("Element not found.");
+  }
+});
